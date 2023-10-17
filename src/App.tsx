373adb1c784a47899439404067cc1bdef8a465a7
RajendraKumarV <rajendravaradaraju@example.com>
2023-10-17 13:16:06 +0530

    reading data from common file type.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import React,{useState,useEffect} from 'react'
 import MovieList from './MovieList';
 import ErrorBoundary from "./ErrorBoundary";
 import "./App.css";
+import axios from 'axios';
+import { MovieData } from './type';
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY || '0ed9e5583ee0385087dff929f46a1b21';
+  const apiUrl = "https://api.themoviedb.org/3/movie/550";
+  const [movie, setMovie] = useState<MovieData | null>(null);
+
+  useEffect(() => {
+    axios.get(`${apiUrl}?api_key=${apiKey}`)
+      .then((response) => {
+        setMovie(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching movie data:', error);
+      });
+  }, [apiKey, movie]);
   return (
     <div className="App">
       <h1>Movie List</h1>
       <ErrorBoundary>
-        <MovieList />
+      <MovieList movieData={movie} />
       </ErrorBoundary>
     </div>
   );
--- src/MovieList.tsx
+++ src/MovieList.tsx
@@ -1,57 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { FC } from "react";
 import "./App.css";
+import { MovieData } from './type';
 
-function MovieList() {
-  interface MovieData {
-    adult: boolean;
-    backdrop_path: string;
-    belongs_to_collection: any; // Define a more specific type if available
-    budget: number;
-    genres: { id: number; name: string }[];
-    homepage: string;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    production_companies: { id: number; logo_path: string | null; name: string; origin_country: string }[];
-    production_countries: { iso_3166_1: string; name: string }[];
-    release_date: string;
-    revenue: number;
-    runtime: number;
-    spoken_languages: { english_name: string; iso_639_1: string; name: string }[];
-    status: string;
-    tagline: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  }
-
-  const apiKey = process.env.REACT_APP_API_KEY || '0ed9e5583ee0385087dff929f46a1b21';
-  const apiUrl = "https://api.themoviedb.org/3/movie/550";
-  const [movie, setMovie] = useState<MovieData | null>(null);
-
-  useEffect(() => {
-    axios.get(`${apiUrl}?api_key=${apiKey}`)
-      .then((response) => {
-        setMovie(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching movie data:', error);
-      });
-  }, [apiKey, movie]);
-
-  if (!movie) {
+const MovieList:FC<{ movieData: MovieData | null }> = ({ movieData }) => {
+  if (!movieData) {
     return <p>Loading...</p>;
   }
   return (
-    <div className="App">
-      <h1>{movie.title}</h1>
-      <p>{movie.overview}</p>
+    <div>
+      <h1>{movieData.title}</h1>
+      <p>{movieData.overview}</p>
+      {/* Render other movie details */}
     </div>
   );
 }
