e61b763d2e42b91d7e7f7c557dd785e6b3fcd3cc
RajendraKumarV <rajendravaradaraju@example.com>
2023-10-20 12:32:38 +0530

    Pagination implementation

--- src/Components/MovieList.tsx
+++ src/Components/MovieList.tsx
@@ -1,22 +1,37 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./MovieCard.css";
+import "./Pagination.css";
 import { MovieData } from "../type";
 import { Link } from "react-router-dom";
+
 interface MovieListProps {
   movieData: any;
 }
 
+const itemsPerPage = 10;
+
 const MovieList: FC<MovieListProps> = ({ movieData }) => {
+  const [currentPage, setCurrentPage] = useState(1);
   if (!movieData || !movieData.results) {
     return <p>Loading...</p>;
   }
 
-  const movies = movieData.results;
+  // Calculate the range of items to display based on currentPage
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const movies = movieData.results.slice(startIndex, endIndex);
+
+  const totalPages = Math.ceil(movieData.results.length / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <div className="movie-list">
       {movies.map((movie: MovieData) => (
         <div key={movie.id} className="movie-card">
-            <Link to={`/movie/${movie.id}`}>
+          <Link to={`/movie/${movie.id}`}>
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
@@ -26,6 +41,17 @@ const MovieList: FC<MovieListProps> = ({ movieData }) => {
           </Link>
         </div>
       ))}
+
+      <div className="pagination">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={index + 1 === currentPage ? "active" : ""}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
