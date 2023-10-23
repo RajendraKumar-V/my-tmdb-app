db30dc04e91e2620af61d401afba3fe9998b60af
RajendraKumarV <rajendravaradaraju@example.com>
2023-10-23 10:27:03 +0530

    Tailwind CSS implemented

--- src/Components/MovieList.tsx
+++ src/Components/MovieList.tsx
@@ -1,6 +1,5 @@
 import React, { FC, useState } from "react";
-import "./MovieCard.css";
-import "./Pagination.css";
+//import "../custom.css"; // Import your custom CSS
 import { MovieData } from "../type";
 import { Link } from "react-router-dom";
 
@@ -12,6 +11,7 @@ const itemsPerPage = 10;
 
 const MovieList: FC<MovieListProps> = ({ movieData }) => {
   const [currentPage, setCurrentPage] = useState(1);
+
   if (!movieData || !movieData.results) {
     return <p>Loading...</p>;
   }
@@ -28,26 +28,36 @@ const MovieList: FC<MovieListProps> = ({ movieData }) => {
   };
 
   return (
-    <div className="movie-list">
-      {movies.map((movie: MovieData) => (
-        <div key={movie.id} className="movie-card">
-          <Link to={`/movie/${movie.id}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-              className="movie-poster"
-            />
-            <h2 className="movie-title">{movie.title}</h2>
-          </Link>
-        </div>
-      ))}
-
-      <div className="pagination">
+    <div>
+      <div className="movie-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {movies.map((movie: MovieData) => (
+          <div
+            key={movie.id}
+            className="movie-card bg-white rounded-lg shadow-md"
+          >
+            <Link to={`/movie/${movie.id}`}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                className="movie-poster w-40 h-40 !important"
+              />
+              <h2 className="movie-title text-lg font-bold mt-2 px-2 py-1">
+                {movie.title}
+              </h2>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="pagination mt-4">
         {Array.from({ length: totalPages }).map((_, index) => (
           <button
             key={index}
             onClick={() => handlePageChange(index + 1)}
-            className={index + 1 === currentPage ? "active" : ""}
+            className={`px-3 py-2 rounded-md ${
+              index + 1 === currentPage
+                ? "bg-blue-500 text-white"
+                : "bg-gray-300"
+            }mr-2`}
           >
             {index + 1}
           </button>
